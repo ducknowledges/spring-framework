2c80cd88fb9c6d8ca1c9b8d9ba6fd5be3111ea74
ducknowledges <ducknowledges@gmail.com>
2022-12-18 14:43:25 +0500

    feat[spring-application-configuration]: add quiz entity

---
+++ quiz-spring-application-configuration/src/main/java/com/github/ducknowledges/quiz/domain/Quiz.java
@@ -0,0 +1,67 @@
+package com.github.ducknowledges.quiz.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Quiz {
+
+    private final String question;
+    private final List<String> options;
+    private final String answer;
+
+
+    public Quiz(String question, String rightAnswer) {
+        this(question, rightAnswer, List.of());
+    }
+
+    public Quiz(String question, String answer, List<String> options) {
+        this.question = question;
+        this.options = options;
+        this.answer = answer;
+
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public boolean hasOptions() {
+        return !options.isEmpty();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Quiz quiz = (Quiz) o;
+        return question.equals(quiz.question)
+                && answer.equals(quiz.answer)
+                && options.equals(quiz.options);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(question, answer, options);
+    }
+
+    @Override
+    public String toString() {
+        return "Quiz{"
+                + "question='" + question + '\''
+                + ", rightAnswer='" + answer + '\''
+                + ", answerOptions=" + options
+                + '}';
+    }
+}
