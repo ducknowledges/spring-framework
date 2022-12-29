19ca42531c926241ddeb21e3b8ef2ddfd775627a
ducknowledges <ducknowledges@gmail.com>
2022-12-29 18:33:02 +0500

    refactor[quiz-introduction-to-spring-framework]: refactor quiz dao

--- quiz-introduction-to-spring-framework/src/main/java/com/github/ducknowledges/quiz/dao/QuizDaoImpl.java
+++ quiz-introduction-to-spring-framework/src/main/java/com/github/ducknowledges/quiz/dao/QuizDaoImpl.java
@@ -1,30 +1,32 @@
 package com.github.ducknowledges.quiz.dao;
 
 import com.github.ducknowledges.quiz.domain.Quiz;
+import com.github.ducknowledges.quiz.domain.Record;
 import com.github.ducknowledges.quiz.parser.DataParser;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class QuizDaoImpl implements QuizDao {
 
-    private final DataParser<List<String>> dataParser;
+    private final DataParser dataParser;
 
-    public QuizDaoImpl(DataParser<List<String>> dataParser) {
+    public QuizDaoImpl(DataParser dataParser) {
         this.dataParser = dataParser;
     }
 
     @Override
     public List<Quiz> getQuizzes() {
-        List<List<String>> records = dataParser.parseToRecords();
+        List<Record> records = dataParser.parseToRecords();
         List<Quiz> quizzes = new ArrayList<>();
-        for (List<String> record : records) {
+        for (Record record : records) {
             if (record.size() == 2) {
-                Quiz quiz = new Quiz(record.get(0), record.get(1));
+                Quiz quiz = new Quiz(record.getContentValue(0), record.getContentValue(1));
                 quizzes.add(quiz);
             }
             if (record.size() > 2) {
-                List<String> options = record.subList(2, record.size());
-                Quiz quiz = new Quiz(record.get(0), record.get(1), options);
+                List<String> options = record.getContent().subList(2, record.size());
+                Quiz quiz = new Quiz(record.getContentValue(0), record.getContentValue(1), options);
                 quizzes.add(quiz);
             }
         }
--- quiz-introduction-to-spring-framework/src/test/java/com/github/ducknowledges/quiz/dao/QuizDaoImplTest.java
+++ quiz-introduction-to-spring-framework/src/test/java/com/github/ducknowledges/quiz/dao/QuizDaoImplTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.github.ducknowledges.quiz.domain.Quiz;
+import com.github.ducknowledges.quiz.domain.Record;
 import com.github.ducknowledges.quiz.parser.DataParserCsv;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -18,13 +19,24 @@ class QuizDaoImplTest {
     void shouldReturnQuizzes() {
         List<String> strings1 = List.of("Is this a question?", "yes");
         List<String> strings2 = List.of("Is this a question with options?", "yes", "yes", "no");
-        List<List<String>> records = List.of(strings1, strings2);
         List<Quiz> quizzes = List.of(
-            new Quiz("Is this a question?", "yes"),
-            new Quiz("Is this a question with options?", "yes", List.of("yes", "no"))
+            new Quiz(strings1.get(0), strings1.get(1)),
+            new Quiz(strings2.get(0), strings2.get(1), strings2.subList(2, strings2.size()))
         );
+
+        Record record1 = mock(Record.class);
+        when(record1.size()).thenReturn(2);
+        when(record1.getContentValue(0)).thenReturn(strings1.get(0));
+        when(record1.getContentValue(1)).thenReturn(strings1.get(1));
+
+        Record record2 = mock(Record.class);
+        when(record2.size()).thenReturn(4);
+        when(record2.getContentValue(0)).thenReturn(strings2.get(0));
+        when(record2.getContentValue(1)).thenReturn(strings2.get(1));
+        when(record2.getContent()).thenReturn(strings2);
+
         DataParserCsv parser = mock(DataParserCsv.class);
-        when(parser.parseToRecords()).thenReturn(records);
+        when(parser.parseToRecords()).thenReturn(List.of(record1, record2));
         QuizDao quizDao = new QuizDaoImpl(parser);
         assertThat(quizDao.getQuizzes()).hasSize(2).hasSameElementsAs(quizzes);
     }
