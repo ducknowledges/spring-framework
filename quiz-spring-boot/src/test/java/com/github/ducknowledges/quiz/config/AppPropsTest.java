a5b7ff5857e997d5944090816c8c59d40dc4766e
ducknowledges <ducknowledges@gmail.com>
2023-01-05 20:08:05 +0500

    refactor[spring-boot]: refactor properties test

--- quiz-spring-boot/src/test/java/com/github/ducknowledges/quiz/config/AppPropsTest.java
+++ quiz-spring-boot/src/test/java/com/github/ducknowledges/quiz/config/AppPropsTest.java
@@ -2,7 +2,9 @@ package com.github.ducknowledges.quiz.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,23 +16,25 @@ class AppPropsTest {
 
     @BeforeEach
     void setUp() {
+        Map<String, String> path = new HashMap<>();
+        path.put("en", "quiz_en.csv");
+        path.put("ru_ru", "quiz_ru.csv");
         props = new AppProps();
+        props.setPath(path);
     }
 
     @Test
     @DisplayName("should return ru quiz resource path")
     void shouldGetRuQuizPath() {
-        props.setLocale(new Locale("ru"));
-        props.setRuQuizPath("path");
-        assertThat(props.getResourcePath()).isEqualTo("path");
+        props.setLocale(new Locale("ru_RU"));
+        assertThat(props.getPath()).isEqualTo("quiz_ru.csv");
     }
 
     @Test
     @DisplayName("should return en quiz resource path")
     void shouldGetEnQuizPath() {
         props.setLocale(new Locale("en"));
-        props.setEnQuizPath("path");
-        assertThat(props.getResourcePath()).isEqualTo("path");
+        assertThat(props.getPath()).isEqualTo("quiz_en.csv");
     }
 
     @Test
