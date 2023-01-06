0b60eef39ce05ef94437f36610ccfaf2831d9b8d
ducknowledges <ducknowledges@gmail.com>
2023-01-07 03:22:50 +0500

    refactor[spring-boot]: refactor quiz manager integration test

--- quiz-spring-boot/src/test/java/com/github/ducknowledges/quiz/integrationtest/QuizManagerServiceImplIntegrationTest.java
+++ quiz-spring-boot/src/test/java/com/github/ducknowledges/quiz/integrationtest/QuizManagerServiceImplIntegrationTest.java
@@ -1,51 +1,59 @@
 package com.github.ducknowledges.quiz.integrationtest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.fail;
 
-import com.github.ducknowledges.quiz.QuizApplication;
+import com.github.ducknowledges.quiz.service.IoService;
+import com.github.ducknowledges.quiz.service.IoServiceStream;
+import com.github.ducknowledges.quiz.service.QuizManagerService;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
+@SpringBootTest
 @DisplayName("Integration Test QuizServiceImpl")
 public class QuizManagerServiceImplIntegrationTest {
 
-    private InputStream in;
-    private OutputStream out;
+    private static InputStream in;
+    private static OutputStream out;
+    private static OutputStream err;
 
-    @BeforeEach
-    void setUp() {
-        String userInput = getUserInput();
-        in = new ByteArrayInputStream(userInput.getBytes(StandardCharsets.UTF_8));
-        out = new ByteArrayOutputStream();
-        System.setIn(in);
-        System.setOut(new PrintStream(out));
-    }
-
-    private String getUserInput() {
-        return "firstName" + System.lineSeparator()
+    @BeforeAll
+    static void setUpAll() {
+        String userInput = "firstName" + System.lineSeparator()
             + "lastName" + System.lineSeparator()
             + "yes" + System.lineSeparator()
             + "no" + System.lineSeparator();
+        in = new ByteArrayInputStream(userInput.getBytes(StandardCharsets.UTF_8));
+        out = new ByteArrayOutputStream();
+        err = new ByteArrayOutputStream();
     }
+
+    @TestConfiguration
+    public static class Configuration {
+        @Bean
+        public IoService ioService() {
+            return new IoServiceStream(in, out, err);
+        }
+
+    }
+
+    @Autowired
+    private QuizManagerService quizManagerService;
 
     @Test
     @DisplayName("should complete quiz")
     void shouldCompleteQuiz() {
         StringBuilder expected = new StringBuilder();
-        expected.append(getAppBanner())
-            .append(System.lineSeparator())
+        expected
             .append("Please, introduce yourself!")
             .append(System.lineSeparator())
             .append("Enter firstName:")
@@ -56,24 +64,13 @@ public class QuizManagerServiceImplIntegrationTest {
             .append(System.lineSeparator())
             .append("Is this a question with options?")
             .append(System.lineSeparator())
-            .append("1.yes, 2.no")
+            .append("Options:yes, no")
             .append(System.lineSeparator())
             .append("firstName lastName, congratulations! You pass the quiz")
             .append(System.lineSeparator())
             .append("Score: success answers=1, failing answers=1")
             .append(System.lineSeparator());
-        QuizApplication.main(new String[]{});
+        quizManagerService.run();
         assertThat(out.toString()).isEqualTo(expected.toString());
-    }
-
-    private String getAppBanner() {
-        Path path = Paths.get("src/test/resources/banner-test.txt");
-        String appBanner = "";
-        try {
-            appBanner = Files.readString(path);
-        } catch (IOException e) {
-            fail("Test failed");
-        }
-        return appBanner;
     }
 }
