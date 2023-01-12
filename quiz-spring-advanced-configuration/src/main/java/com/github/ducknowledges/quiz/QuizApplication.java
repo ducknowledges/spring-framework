505d1c732f821aabcf88ecaa4c420e0c16c7e56e
ducknowledges <ducknowledges@gmail.com>
2023-01-12 17:08:19 +0500

    feat[spring-advanced-configuration]: refactor quiz app main class

--- quiz-spring-advanced-configuration/src/main/java/com/github/ducknowledges/quiz/QuizApplication.java
+++ quiz-spring-advanced-configuration/src/main/java/com/github/ducknowledges/quiz/QuizApplication.java
@@ -1,16 +1,14 @@
 package com.github.ducknowledges.quiz;
 
-import com.github.ducknowledges.quiz.service.QuizManagerService;
-import com.github.ducknowledges.quiz.service.QuizManagerServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class QuizApplication {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(QuizApplication.class, args);
-        QuizManagerService quizManagerService = context.getBean(QuizManagerServiceImpl.class);
-        quizManagerService.run();
+        SpringApplication.run(QuizApplication.class, args);
+        //ApplicationContext context = SpringApplication.run(QuizApplication.class, args);
+        //QuizManagerService quizManagerService = context.getBean(QuizManagerServiceImpl.class);
+        //quizManagerService.run();
     }
 }
