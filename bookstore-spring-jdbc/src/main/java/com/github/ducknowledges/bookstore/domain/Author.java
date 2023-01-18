290302dff5e823f6a4c7d1654cf2aa0d8cad4ad6
ducknowledges <ducknowledges@gmail.com>
2023-01-18 21:03:30 +0500

    feat[spring-jdbc]: add author domain

---
+++ bookstore-spring-jdbc/src/main/java/com/github/ducknowledges/bookstore/domain/Author.java
@@ -0,0 +1,44 @@
+package com.github.ducknowledges.bookstore.domain;
+
+import java.util.Objects;
+
+public class Author {
+
+    private final Integer id;
+    private final String name;
+
+    public Author(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Author(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Author author = (Author) o;
+        return Objects.equals(id, author.id) && Objects.equals(name, author.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
