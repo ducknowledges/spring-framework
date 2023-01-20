fbb1fdd07ef227ffa29b13f0d6997a755b3fc7b9
ducknowledges <ducknowledges@gmail.com>
2023-01-20 17:34:42 +0500

    feat[spring-jdbc]: add genre domain

---
+++ bookstore-spring-jdbc/src/main/java/com/github/ducknowledges/bookstore/domain/Genre.java
@@ -0,0 +1,44 @@
+package com.github.ducknowledges.bookstore.domain;
+
+import java.util.Objects;
+
+public class Genre {
+
+    private final Integer id;
+    private final String name;
+
+    public Genre(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Genre(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Genre genre = (Genre) o;
+        return Objects.equals(id, genre.id) && Objects.equals(name, genre.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
---
+++ bookstore-spring-jdbc/src/test/java/com/github/ducknowledges/bookstore/domain/GenreTest.java
@@ -0,0 +1,36 @@
+package com.github.ducknowledges.bookstore.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Class Genre")
+class GenreTest {
+    @Test
+    @DisplayName("correctly created by the constructor")
+    void shouldHaveCorrectConstructor() {
+        Genre genre = new Genre(1, "name");
+        assertAll(
+            () -> assertThat(genre.getId()).isEqualTo(1),
+            () -> assertThat(genre.getName()).isEqualTo("name")
+        );
+    }
+
+    @Test
+    @DisplayName("correctly created by the constructor without id")
+    void shouldHaveCorrectConstructorWithoutId() {
+        Genre genre = new Genre("name");
+        assertAll(
+            () -> assertThat(genre.getName()).isEqualTo("name")
+        );
+    }
+
+    @Test
+    @DisplayName("should has correct equality")
+    void shouldHasCorrectEquality() {
+        assertThat(new Genre(1, "name"))
+            .isEqualTo(new Genre(1, "name"));
+    }
+}
