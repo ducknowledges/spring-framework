fb8f0ac035422b9f290f154fc1ceeeb00ad32d96
ducknowledges <ducknowledges@gmail.com>
2023-01-24 22:59:37 +0500

    feat[spring-jdbc]: add book print formatter

---
+++ bookstore-spring-jdbc/src/main/java/com/github/ducknowledges/bookstore/printformatter/BookPrintFormatter.java
@@ -0,0 +1,33 @@
+package com.github.ducknowledges.bookstore.printformatter;
+
+import com.github.ducknowledges.bookstore.domain.Book;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("bookFormatter")
+public class BookPrintFormatter implements PrintFormatter<Book> {
+
+    @Override
+    public String format(Book book) {
+        int id = book.getId();
+        String name = book.getName();
+        String author = book.getAuthor().getName();
+        String genre = book.getGenre().getName();
+        return String.format(
+            "id: %d name: %s author: %s genre: %s",
+            id, name, author, genre);
+    }
+
+    @Override
+    public String format(List<Book> books) {
+        StringBuilder stringBuilder = new StringBuilder("Books:" + System.lineSeparator());
+        for (Book book : books) {
+            stringBuilder
+                .append(format(book))
+                .append(System.lineSeparator());
+        }
+        return stringBuilder.toString();
+    }
+}
