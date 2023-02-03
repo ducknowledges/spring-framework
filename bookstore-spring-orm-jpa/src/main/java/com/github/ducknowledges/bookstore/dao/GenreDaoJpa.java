13a27f53015011796f963d28da84db79989c70f2
ducknowledges <ducknowledges@gmail.com>
2023-02-03 05:18:50 +0500

    feat[spring-orm-jpa]: refactor genre dao

--- bookstore-spring-orm-jpa/src/main/java/com/github/ducknowledges/bookstore/dao/GenreDaoJpa.java
+++ bookstore-spring-orm-jpa/src/main/java/com/github/ducknowledges/bookstore/dao/GenreDaoJpa.java
@@ -4,11 +4,13 @@ import com.github.ducknowledges.bookstore.domain.Genre;
 import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class GenreDaoJpa implements GenreDao {
 
+    @PersistenceContext
     private final EntityManager manager;
 
     public GenreDaoJpa(EntityManager manager) {
@@ -21,7 +23,10 @@ public class GenreDaoJpa implements GenreDao {
     }
 
     @Override
-    public List<Genre> readAll() {
-        return manager.createQuery("select g from Genre g", Genre.class).getResultList();
+    public List<Genre> readAll(int from, int size) {
+        return manager.createQuery("select g from Genre g", Genre.class)
+            .setFirstResult(from < 1 ? 0 : from - 1)
+            .setMaxResults(size < 1 ? 0 : size)
+            .getResultList();
     }
 }
