262389c5501727f7f90653c2c465c4095700ce7e
ducknowledges <ducknowledges@gmail.com>
2023-02-05 01:30:18 +0500

    refactor[spring-orm-jpa]: refactor book comments commands

--- bookstore-spring-orm-jpa/src/main/java/com/github/ducknowledges/bookstore/command/BookCommentCommand.java
+++ bookstore-spring-orm-jpa/src/main/java/com/github/ducknowledges/bookstore/command/BookCommentCommand.java
@@ -1,14 +1,10 @@
 package com.github.ducknowledges.bookstore.command;
 
-import com.github.ducknowledges.bookstore.domain.Author;
-import com.github.ducknowledges.bookstore.domain.Book;
 import com.github.ducknowledges.bookstore.domain.BookComment;
-import com.github.ducknowledges.bookstore.domain.Genre;
 import com.github.ducknowledges.bookstore.printformatter.PrintFormatter;
 import com.github.ducknowledges.bookstore.service.BookCommentService;
 import com.github.ducknowledges.bookstore.service.BookService;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
@@ -31,34 +27,30 @@ public class BookCommentCommand {
     @ShellMethod(value = "Create comment command", key = "create-comment")
     public String createComment(@ShellOption String content,
                                 @ShellOption long bookId) {
-        Optional<Book> book = bookService.getBook(bookId);
-        if (book.isEmpty()) {
-            return  "Book does not exist";
-        }
-        BookComment createdComment = commentService.createComment(
-            new BookComment(content, book.get())
-        );
-        return "Comment was created: " + printFormatter.format(createdComment);
+        return bookService.getBook(bookId)
+            .map(b -> {
+                BookComment comment = new BookComment(content, b);
+                BookComment createdComment = commentService.createComment(comment);
+                return "Comment was created: " + printFormatter.format(createdComment);
+            }).orElse("Book does not exist");
     }
 
     @ShellMethod(value = "Read comments command", key = "read-comment")
     public String getComment(@ShellOption long commentId) {
-        Optional<BookComment> comment = commentService.getComment(commentId);
-        if (comment.isPresent()) {
-            return printFormatter.format(comment.get());
-        }
-        return "Comment doesn't exist";
+        return commentService.getComment(commentId)
+            .map(printFormatter::format)
+            .orElse("Comment doesn't exist");
     }
 
     @ShellMethod(value = "Read all comments command", key = "read-comments")
     public String getComments(@ShellOption int from,
-                           @ShellOption int size) {
+                              @ShellOption int size) {
         List<BookComment> comments = commentService.getComments(from, size);
         return printFormatter.format(comments);
     }
 
     @ShellMethod(value = "Read all comments by book id command", key = "read-comments")
-    public String getComments(@ShellOption long bookId) {
+    public String getCommentsByBookId(@ShellOption long bookId) {
         List<BookComment> comments = commentService.getCommentsByBookId(bookId);
         return printFormatter.format(comments);
     }
@@ -66,14 +58,13 @@ public class BookCommentCommand {
     @ShellMethod(value = "Update comment content by id command", key = "update-comment")
     public String update(@ShellOption long commentId,
                          @ShellOption String content) {
-        Optional<BookComment> comment = commentService.getComment(commentId);
-        if (comment.isEmpty()) {
-            return  "Comment does not exist";
-        }
-        BookComment updatedComment = commentService.update(
-            new BookComment(commentId, content, comment.get().getBook())
-        );
-        return "Comment was updated: " + printFormatter.format(updatedComment);
+        return commentService.getComment(commentId)
+            .map(comment -> {
+                comment.setContent(content);
+                return commentService.update(comment);
+            })
+            .map(comment -> "Comment was updated: " + printFormatter.format(comment))
+            .orElse("Comment does not exist");
     }
 
     @ShellMethod(value = "Delete book command", key = "delete-comment")
