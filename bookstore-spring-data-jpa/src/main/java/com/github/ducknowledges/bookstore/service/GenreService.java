056386c5c87d6d0243223fe8ca1e0e99f278920a
ducknowledges <ducknowledges@gmail.com>
2023-02-07 03:02:41 +0500

    feat[spring-data-jpa]: add genre service

---
+++ bookstore-spring-data-jpa/src/main/java/com/github/ducknowledges/bookstore/service/GenreService.java
@@ -0,0 +1,12 @@
+package com.github.ducknowledges.bookstore.service;
+
+import com.github.ducknowledges.bookstore.domain.Genre;
+import java.util.List;
+import java.util.Optional;
+
+public interface GenreService {
+
+    Optional<Genre> getGenre(long id);
+
+    List<Genre> getGenres(long fromId, long toId);
+}
