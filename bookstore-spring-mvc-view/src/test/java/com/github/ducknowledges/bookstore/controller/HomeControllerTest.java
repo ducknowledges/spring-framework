8a2efbf3bcfa3575537a5abf65b2cdce3b05f3a4
ducknowledges <ducknowledges@gmail.com>
2023-02-15 16:30:28 +0500

    feat[spring-mvc-view]: add root controller

---
+++ bookstore-spring-mvc-view/src/test/java/com/github/ducknowledges/bookstore/controller/HomeControllerTest.java
@@ -0,0 +1,48 @@
+package com.github.ducknowledges.bookstore.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import com.github.ducknowledges.bookstore.service.AuthorService;
+import com.github.ducknowledges.bookstore.service.BookService;
+import com.github.ducknowledges.bookstore.service.GenreService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AuthorService authorService;
+
+    @MockBean
+    private GenreService genreService;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    @DisplayName("should correct handle get / request")
+    void shouldCorrectHandleRootPageGetRequest() throws Exception {
+        when(authorService.count()).thenReturn(1L);
+        when(genreService.count()).thenReturn(1L);
+        when(bookService.count()).thenReturn(1L);
+
+        mvc.perform(get("/")).andExpect(status().isOk())
+            .andExpect(view().name("index"))
+            .andExpect(model().attribute("bookCounter", 1L))
+            .andExpect(model().attribute("authorCounter", 1L))
+            .andExpect(model().attribute("genreCounter", 1L));
+    }
+}
