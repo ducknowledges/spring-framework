57a075e5e0326a272ef1097d33c26ef6c044cdca
ducknowledges <ducknowledges@gmail.com>
2023-02-21 18:08:22 +0500

    feat[spring-mvc-rest]: add book service

---
+++ bookstore-spring-mvc-rest/src/main/java/com/github/ducknowledges/bookstore/service/BookService.java
@@ -0,0 +1,20 @@
+package com.github.ducknowledges.bookstore.service;
+
+import com.github.ducknowledges.bookstore.domain.Book;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface BookService {
+    Book create(Book book);
+
+    Optional<Book> getBook(long id);
+
+    Page<Book> getBooks(Pageable pageable);
+
+    public Book save(Book book);
+
+    void deleteById(long id);
+
+    long count();
+}
---
+++ bookstore-spring-mvc-rest/src/main/java/com/github/ducknowledges/bookstore/service/BookServiceImpl.java
@@ -0,0 +1,51 @@
+package com.github.ducknowledges.bookstore.service;
+
+import com.github.ducknowledges.bookstore.dao.BookDao;
+import com.github.ducknowledges.bookstore.domain.Book;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookDao bookDao;
+
+    public BookServiceImpl(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    @Override
+    public Book create(Book book) {
+        return bookDao.save(book);
+    }
+
+    @Override
+    public Optional<Book> getBook(long id) {
+        return bookDao.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Book> getBooks(Pageable pageable) {
+        return bookDao.findAll(pageable);
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookDao.save(book);
+    }
+
+    @Override
+    public void deleteById(long id) {
+        bookDao.deleteById(id);
+    }
+
+    @Override
+    public long count() {
+        return bookDao.count();
+    }
+}
