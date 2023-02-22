5637659e1d285bd84763b9667c1ae5627830b682
ducknowledges <ducknowledges@gmail.com>
2023-02-22 16:12:35 +0500

    feat[spring-mvc-rest]: add genre mapper

---
+++ bookstore-spring-mvc-rest/src/main/java/com/github/ducknowledges/bookstore/mapper/GenreMapper.java
@@ -0,0 +1,15 @@
+package com.github.ducknowledges.bookstore.mapper;
+
+import com.github.ducknowledges.bookstore.domain.Genre;
+import com.github.ducknowledges.bookstore.dto.PageResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GenreMapper implements PageResponseMapper<Genre, Genre> {
+
+    public PageResponseDto<Genre> getPageResponseDto(Page<Genre> genrePage) {
+        return new PageResponseDto<>(genrePage);
+    }
+
+}
