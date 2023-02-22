8f88292ac6dd5010c44e762efbf2ed85851d3f9f
ducknowledges <ducknowledges@gmail.com>
2023-02-22 16:12:51 +0500

    feat[spring-mvc-rest]: add book comment mapper

---
+++ bookstore-spring-mvc-rest/src/main/java/com/github/ducknowledges/bookstore/mapper/BookCommentMapper.java
@@ -0,0 +1,16 @@
+package com.github.ducknowledges.bookstore.mapper;
+
+import com.github.ducknowledges.bookstore.domain.BookComment;
+import com.github.ducknowledges.bookstore.dto.BookCommentDto;
+import com.github.ducknowledges.bookstore.dto.PageResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookCommentMapper implements PageResponseMapper<BookComment, BookCommentDto> {
+
+    public PageResponseDto<BookCommentDto> getPageResponseDto(Page<BookComment> commentPage) {
+        return new PageResponseDto<>(commentPage.map(BookCommentDto::new));
+    }
+
+}
