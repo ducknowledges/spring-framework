30a91ff97dd16d55d3ead1242750a09e27dc6a45
ducknowledges <ducknowledges@gmail.com>
2023-02-27 04:07:00 +0500

    fix[spring-mvc-rest]: fix error redirect controller

--- bookstore-spring-mvc-rest/src/main/java/com/github/ducknowledges/bookstore/controller/ErrorRedirectController.java
+++ bookstore-spring-mvc-rest/src/main/java/com/github/ducknowledges/bookstore/controller/ErrorRedirectController.java
@@ -2,12 +2,12 @@ package com.github.ducknowledges.bookstore.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ErrorRedirectController implements ErrorController {
 
-    @RequestMapping("/error")
+    @GetMapping("/error")
     public String handleError() {
         return "forward:/";
     }
